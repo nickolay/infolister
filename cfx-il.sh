7a02a365b6e992765724e06635c8835d686dcef9
Nickolay Ponomarev <asqueella@gmail.com>
2010-05-23 21:07:45 +0400

    Switch to cfx (from jetpack SDK) for packaging/testing InfoLister: 1) Move the extension itself to extension/ subfolder. 2) set up cfx to use that subfolder as an extension template (in cfx-il.sh) 3) Change install.rdf, so that cfx xpi doesn't choke on it 4) add package.json for cfx 5) Remove build.sh and related machinery

---
+++ cfx-il.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cfx -a firefox -t extension $1
--- config_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-APP_NAME=infolister
-CHROME_PROVIDERS="content locale skin"
-CLEAN_UP=1
-ROOT_DIRS="defaults components"
-ROOT_FILES="license.txt"
-BEFORE_BUILD=./update_builddate.sh
-AFTER_BUILD=
