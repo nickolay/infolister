db0cd01e9952bb5ba5e1843992ad2690749c3be4
Nickolay Ponomarev <asqueella@gmail.com>
2010-12-20 03:24:47 +0300

    update cfx invocations to the syntax allowed in the newer versions of the Addon SDK

--- cfx-il.sh
+++ cfx-il.sh
@@ -2,7 +2,8 @@
 
 cp ~/dev/jetpack-sdk/python-lib/cuddlefish/app-extension/components/harness.js extension/components || exit $?
 
-cfx -a firefox -t extension $1 $2 -P `pwd`/profile
+cfx -a firefox --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
+#cfx -a firefox -b /Applications/Minefield.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
 if [ "$1" = "xpi" ]
 then
   echo "Remember to add the 'defaults' to the XPI! (bug 559306)"
