8bb9f4c61ce5ecb610321b16497af3490ff2a174
Nickolay Ponomarev <asqueella@gmail.com>
2011-01-06 22:51:09 +0300

    make the extension start up and pass tests on current Minefield (no adjustments for the new Addons manager yet)

---
+++ minefield.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp ~/dev/jetpack-sdk/python-lib/cuddlefish/app-extension/components/harness.js extension/components || exit $?
+
+#cfx -a firefox --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
+cfx -a firefox -b /Applications/Minefield.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
+if [ "$1" = "xpi" ]
+then
+  echo "Remember to add the 'defaults' to the XPI! (bug 559306)"
+  echo "Also, you need to update the contractID for the harness component in infolister-service (bug 567642)"
+fi
