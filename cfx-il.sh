9f7e44b5da0faa8721e20b26d15a061f619d7104
Nickolay Ponomarev <asqueella@gmail.com>
2011-01-10 21:30:17 +0300

    [mq]: move-to-subfolder.patch

--- cfx-il.sh
+++ cfx-il.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cp ~/dev/jetpack-sdk/python-lib/cuddlefish/app-extension/components/harness.js extension/components || exit $?
+cp addon-sdk/python-lib/cuddlefish/app-extension/components/harness.js infolister/extension/components || exit $?
+pushd infolister || exit $?
 
-cfx -a firefox --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile-3.6
+cfx -a firefox --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/../profile-3.6
 #cfx -a firefox -b /Applications/Minefield.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
 if [ "$1" = "xpi" ]
 then
   echo "Remember to add the 'defaults' to the XPI! (bug 559306)"
   echo "Also, you need to update the contractID for the harness component in infolister-service (bug 567642)"
 fi
+
+popd
--- minefield.sh
+++ minefield.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-cp ~/dev/jetpack-sdk/python-lib/cuddlefish/app-extension/components/harness.js extension/components || exit $?
+cp addon-sdk/python-lib/cuddlefish/app-extension/components/harness.js infolister/extension/components || exit $?
 
 #export NSPR_LOG_MODULES=nsHttp:5,nsSocketTransport:5,nsHostResolver:5,nsHttpChannel:5
 #export NSPR_LOG_FILE=~/dev/infolister-googlecode/log.txt
 
+pushd infolister
+
 #cfx -a firefox --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
-cfx -a firefox -b /Applications/Minefield.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
+cfx -a firefox -b /Applications/Minefield.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/../profile
 #cfx -a firefox -b ~/dev/mozilla-work/src/obj-ff-debug/dist/MinefieldDebug.app --templatedir extension $1 $2 $3 $4 --profiledir `pwd`/profile
 if [ "$1" = "xpi" ]
 then
   echo "Remember to add the 'defaults' to the XPI! (bug 559306)"
   echo "Also, you need to update the contractID for the harness component in infolister-service (bug 567642)"
 fi
+
+popd
