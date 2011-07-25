bb8e4b86cf61c661c77d527b3e5d11b01c9bd174
Nickolay Ponomarev <asqueella@gmail.com>
2011-07-26 00:42:16 +0400

    bump version to 0.10.4a2, TB maxVersion to 6.*, update the site, and add a dummy main.js to trick the linker.

---
+++ infolister/lib/main.js
@@ -0,0 +1,8 @@
+if (0) {
+  // temporary hack to trick cfx into linking the necessary modules
+  // into the extension during 'cfx run' and 'cfx xpi'
+  require("format");
+  require("common");
+  require("addonsProvider");
+  require("collect");
+}
