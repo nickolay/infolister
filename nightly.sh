4bb8fc968caa2d00f97dfe98c7867525f74e662a
Nickolay Ponomarev <asqueella@gmail.com>
2015-03-22 19:08:50 +0300

    change directory hierarchy in repo to match the XPI dir structure

---
+++ nightly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#export NSPR_LOG_MODULES=nsHttp:5,nsSocketTransport:5,nsHostResolver:5,nsHttpChannel:5
+#export NSPR_LOG_FILE=~/dev/infolister/log.txt
+
+pushd `dirname $0`
+echo "`pwd`/infolister/extension/" > profile/extensions/\{3f0da09b-c1ab-40c5-8d7f-53f475ac3fe8\}
+/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin -foreground -no-remote -profile `pwd`/profile
+popd
