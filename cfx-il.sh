19219280847714d814190038ed078270da64658a
Nickolay Ponomarev <asqueella@gmail.com>
2015-07-10 22:39:06 +0200

    move site/ to the root of the gh-pages branch.

--- cfx-il.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp addon-sdk/python-lib/cuddlefish/app-extension/components/harness.js infolister/extension/components || exit $?
-pushd infolister || exit $?
-
-cfx -a firefox $1 $2 $3 $4 --profiledir `pwd`/../profile-3.6
-#cfx -a firefox -b /Applications/Minefield.app $1 $2 $3 $4 --profiledir `pwd`/profile
-
-popd
--- infolister/tests/test-upload-check-results.sjs
+++
@@ -1,7 +0,0 @@
-// This is a server script for httpd.js.
-// Used to get the results PUT at "/test-upload-put-target.sjs" in test-upload.js.
-function handleRequest(request, response)
-{
-  response.setHeader("Content-Type", "text/plain", false);
-  response.write(getSharedState("test-upload: PUT data"));
-}
