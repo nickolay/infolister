a539567c983ed6e5f2efcf93e8f07edaa9e86643
Nickolay Ponomarev <asqueella@gmail.com>
2016-08-06 19:45:12 +0300

    fix JavaScript warning: chrome://infolister/content/common-shim.js, line 4: expression closures are deprecated

--- extension/content/common-shim.js
+++ extension/content/common-shim.js
@@ -1,5 +1,6 @@
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm"); /* XPCOMUtils */
 Components.utils.import("resource://infolister/common.js", this); /* InfoListerWindows, ILHelpers, ILPrefs */
-XPCOMUtils.defineLazyGetter(this, "InfoListerService", function()
-    Components.classes["@mozilla.doslash.org/infolister/service;1"].
-      getService(Components.interfaces.nsISupports).wrappedJSObject);
+XPCOMUtils.defineLazyGetter(this, "InfoListerService", function() {
+    return  Components.classes["@mozilla.doslash.org/infolister/service;1"].
+      getService(Components.interfaces.nsISupports).wrappedJSObject
+  });
